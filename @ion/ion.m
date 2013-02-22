9b55675a054e2337f5088977a5748075ebc865f1
Lewis A. Marshall <lewis.a.marshall@gmail.com>
2013-02-21 17:33:59 -0800

    Added both of the initial scripts, for ions and solutions.

---
+++ @ion/ion.m
@@ -0,0 +1,313 @@
+classdef ion < handle
+	% A class for ions dissolved in solution. 
+    % Draws significantly on Bagha Electrophoresis 2010
+	% "Ionic strength effects on electrophoretic focusing and separations"
+	
+	% The ion is defined by a name, and a set of charge states (z).
+	% Each charge state must have an associated acidity constant (pKa).
+	% Each charge state must have an associated fully ionized mobility (fi_mobility).
+    properties
+        name;
+        z=0;
+        pKa;
+        fi_mobility; %Expected in m^2/V/s.  
+    end
+    
+	% Properties only accessible by the funciton itself. 
+	% These are constants and should not change.
+	% Eventually, T may be  removed from the constants list.
+	
+    properties(Constant = true, GetAccess = 'private')
+        F=9.6485E4;       	% Faraday's const.[C/mol]
+        Lpm3=1000;			% Conversion from liters to m^3
+		T=298;				% Temperature, in Kalvin
+		% The following are constants in eqtn 6 of Bahga 2010. 
+		Adh=0.5102; 		% L^1/2 / mol^1/2, approximate for room temperature
+		aD=1.5*sqrt(2); 	% mol^-1/2 mol^-3/2, approximation
+    end
+    
+    methods
+        function obj=ion(name, z, pKa, fi_mobility)
+			%Class constructor. 
+            %Initialize the object, with checks on the form of the variables.
+            if(nargin ==4 ) %
+				%check that the name is a string
+                if ischar(name)
+                    obj.name=name;
+                else
+                    error('Ion name must be a string.')
+                end
+				
+                % Check that z is a vector of integers
+                if z==int8(z) & isvector(z)
+                    obj.z=double(z);
+                else
+                    error('Charge states must be a vector of integers.')
+                end
+                
+				% Check that the pKa is a vector of numbers of the same length as z. 
+                if isvector(pKa) && length(obj.z)==length(pKa) &&isnumeric(fi_mobility)
+                    obj.pKa=double(pKa);
+                else
+                    error ('pKas must be a numeric vector the same size as charge vector.')
+                end
+                
+				%Check that the fully ionized mobility is a vector of numbers the same size as z. 
+                if isvector(fi_mobility) && length(obj.z)==length(fi_mobility) && isnumeric(fi_mobility)
+                    obj.fi_mobility=double(fi_mobility);
+                else
+                    error ('Fully ionized mobilities must be a numeric vector the same size as charge vector.')
+                end
+                
+				% Force the sign of the fully ionized mobilities to match the sign of the charge. 
+				% This command provides a warning, which you can suppress, with, for example, 
+				% warning('off','all');
+                if ~all(sign(obj.z)==sign(obj.fi_mobility))
+                    obj.fi_mobility=abs(obj.fi_mobility).*double(sign(obj.z));
+                    warning('Forcing fully ionized mobility signs to match charge signs.')
+                end
+                
+            else
+				% If there are not four inputs, the initialization fails.
+                error ('Not enough inputs to describe an ion.')
+            end
+            
+            % After storing the ion properties, ensure that the properties are sorted in order of charge. 
+			% All other ion methods assume that the states will be sorted by charge. 
+            obj=obj.z_sort(); 
+        end
+		
+        function L=get_L(obj, I)
+			% Creates a vector of the products of acidity constants, for use in the pH calculating routine.
+			% It can use ionic strength correction if an ionic strength is specified. Otherwise. It uses 
+			% uncorrected acidity coefficients. 
+			
+			%%%%%%%%%%%%%%%%%%
+			%This could be rewritten to use the z0 method. 
+			%%%%%%%%%%%%%%%%%%
+			
+            L=zeros(size(obj.pKa));
+			
+			if ~exist('I', 'var')
+				Ka=obj.Ka;
+			else
+				Ka=obj.get_Ka_eff(I);
+			end
+			
+			
+            for i = 1:length(L)
+                if obj.z(i)<0
+                    L(i)=prod(Ka(obj.z>=obj.z(i) & obj.z<0));
+                else
+                    L(i)=prod(Ka(obj.z<=obj.z(i) & obj.z>=0).^(-1));
+                end
+            end
+        end
+        
+        function i_frac=ionization_fraction(obj, pH, I ,index)
+        	% This function takes the ion and the pH and the ionic strength. 
+			% It computes the fraction of ion in each of the ionization z states
+			% in z. If index is specified, it will only return the ionization 
+			% in ionization state z(index). 
+			
+			% If ionic strength is not specified, set it to zero. 
+			if ~exist('I', 'var')
+				I=0;
+			end
+			
+			% Sanitize the pH input. 
+			if ~isnumeric(pH)
+				error('pH should be a number.')
+			end
+			
+			if length(pH)~=1
+				pH=pH(1); 
+				warning('Ionization fraction only takes a single pH. Using pH(1).')
+			end
+			
+			% Get the vector of products of acidity constants.
+            L=obj.get_L(I);
+			% Compute the concentration of H+ from the pH.
+            cH=10.^(-pH)';
+            
+			% Calculate the denominator of the function for ionization fraction.
+            i_frac_denom=1+sum(L.*bsxfun(@power, cH, obj.z),2);
+            
+			%Calculate the vector of ionization fractions
+            i_frac=L.*bsxfun(@power, cH, obj.z)./i_frac_denom;
+			
+			% If index is specified, return only the ionization fraction of z(i).
+			if exist('index', 'var')
+				try
+					i_frac=i_frac(index);
+				catch
+					% Send a warning if the index is meaningless. 
+					% Still return the vector. 
+					warning('Specified index is out of bounds.')
+				end
+			end
+        end
+        
+        function m_cond=molar_conductivity(obj, pH, I)
+            % This function takes the ion and the pH and computes the molar
+            % conductivity. This function can take an ionic strength. 
+			% Provides conducitivity in Siemens per meter per mole.
+			
+			%%%%%%%%%%%%%%%%
+			% Does not yet use the ionic strength corrected mobilities.
+			%%%%%%%%%%%%%%%%
+            
+			if ~exist('I', 'var')
+				I=0;
+			end
+			
+            i_frac=ionization_fraction(obj, pH, I);
+            
+            m_cond=sum(obj.F*obj.z.*i_frac.*obj.fi_mobility*obj.Lpm3);
+            
+        end
+        
+        function eff_mobility=effective_mobility(obj, pH, I)
+            % This function takes the ion and a pH and an ionic strength. It calls the ionization
+            % fraction function and uses this information to compute the
+            % effective mobility of the ion. 
+			
+			%%%%%%%%%
+			% This function uses ionic strength for ionization fraction 
+			% calculation, but it does not use it for fi_mobility.
+			%%%%%%%%%
+			
+			if ~exist('I', 'var')
+				I=0;
+			end
+			
+            for i = 1:length(pH)
+                i_frac=ionization_fraction(obj, pH(i), I);
+                eff_mobility(i)=sum(i_frac.*obj.fi_mobility);
+            end
+			
+        end
+        
+        function obj=z_sort(obj)
+			% This function sorts the charge state list (and the associated pKas)
+			% by the value of the charge state (from negative to positive). This
+			% is required for other methods to work correclty.
+			
+			% Sort the charges. Store the index order.
+          	[obj.z,Index]=sort(obj.z);
+			% Sort the pKas by the stored index order.
+         	obj.pKa=obj.pKa(Index);
+			% Sort the mobilities by the stored index order.
+            obj.fi_mobility=obj.fi_mobility(Index);
+			
+			% This section will check each charge state to see if it is complete.
+			% That is, if there is a charge state -2, there must be a charge state
+			% -1. if there is a charge state +3, there must be a +2 and a +1.
+			warn=0;
+			for i=1:length(obj.z)
+				if obj.z(i) < -1 && obj.z(i+1)~=obj.z(i)+1;
+					warn=1;
+				elseif obj.z(i)>1 && obj.z(i-1)~=obj.z(i)-1;
+					warn=1;
+				end
+			end
+			%Send a single warning if any charge state is missing
+			if warn==1;
+				warning('Charge states may be missing...')
+			end		
+        end
+
+		function Ka=Ka(obj)
+			% Calculates the Kas from the pKas. 
+			Ka=10.^(-obj.pKa);
+		end
+		
+		function z0=get_z0(obj)
+			% Calls the list of charge states, but inserts 0 in the appropriate position. 
+			
+			z0=[0, obj.z];
+			z0=sort(z0);
+		end
+
+		function Ka_eff=get_Ka_eff(obj, I)
+			% Uses the ionic strength correction function from
+			% Dubye-Huckle theory to calculate the activity coefficients, 
+			% and from this, compute the effective Ka values for the ion. 
+			
+			% If the ionic strength is zero, simply return the Ka's. 
+			if I==0;
+				Ka_eff=obj.Ka; 
+				return
+			end
+			
+			% Store the Ka values, rather than continually recalculating them.
+			Ka=obj.Ka;
+			% Make the effective Ka vector the same size as the Ka vector.
+			Ka_eff=Ka;
+			
+			% Call the list of charge states, including 0. 
+			% This is required b/c you need the activity of the uncharged species.
+            z_list=obj.get_z0;
+			 
+			% There are two coefficients that are used repeatedly.
+			% Specified in Bahga. 
+			A=obj.Adh*sqrt(I)/(1+obj.aD*sqrt(I));
+			B=0.1*I;
+			
+			% Calculate (the log of) the activity coefficients. 
+			% First, the H+,
+			gam_h=B-A;
+			% then the ion states.
+			gam_i=z_list.^2*(B-A);
+			
+			% Convert both to actual activity coefficients.
+			gam_h=10^gam_h;
+			gam_i=10.^gam_i;
+			
+			% For each acidity coefficient, get the effective 
+			% coefficienty by multiplying by activities.
+			for i=1:length(Ka_eff)
+				Ka_eff(i)=Ka(i)*gam_i(i+1)/gam_i(i)/gam_h;
+			end
+
+		end
+		
+		function fi_mobility_effective=get_fi_mobility_effective(obj, I)
+			% If only an ionic strength is specified, use the Robinson-Stokes 
+			% correction to calculate a new fully ionized mobility. 
+			
+			% If a solution object is supplied, use the full onsager fouss correction. 
+			
+			if isnumeric(I) && I>=0
+				% Robinson-Stokes correction. Currently using the ionic strength where Bahga 2010
+				% uses twice the ionic strength. This appears to work, and follows the SPRESSO implimentation. 
+				% Likely typo in paper. 
+				A=0.2297;
+				B=31.410e-9;
+				fi_mobility_effective=obj.fi_mobility-(A.*obj.fi_mobility+B.*sign(obj.z))*sqrt(I)/(1+1.5*sqrt(I));
+				
+			elseif strcmp(class(I), 'solution')
+				TMP=I.add_ion(obj, 0);
+				fi_mobility_effective=TMP.get_factor;
+				fi_mobility_effective=fi_mobility_effective(end-length(obj.z)+1:end);
+			else
+				error('Ionic strength must be specified as either a positive scalar value or a solution object.')
+			end
+		end
+		
+    end %End of Methods
+    
+end %End of Object
+
+
+% Devalued Methods:
+
+%function obj=add_state(obj, new_z, new_pKa, new_fi_mobility)
+% %This function is used to add a new charge state to an existing ion. 
+% %It is rarely used...Devalued for now.
+% 
+%             obj.z=[obj.z, new_z];
+%             obj.pKa=[obj.pKa, new_pKa];
+%             obj.fi_mobility=[obj.fi_mobility, new_fi_mobility];
+%             obj=obj.z_sort();
+%end
---
+++ @solution/solution.m
@@ -0,0 +1,343 @@
+classdef solution < handle
+    % A for a solution containing one or more ions.
+    
+    properties(Constant = true, GetAccess = 'private')
+        F=9.65E4;           % Faraday's const.[C/mol]
+        Rmu=8.31;           % Universal gas const. [J/mol*K]
+        Temp=298;           % Temperature [K]
+        Kw=1E-14;           % Water equilibrium constant
+        muH=362E-9%/obj.F;   % Mobility of Hydronium   % [m^2/s*V]/F --> [mol*s/Kg]
+        muOH=205E-9%/obj.F;  % Mobility of Hydroxide   % [m^2/s*V]/F --> [mol*s/Kg]
+        Lpm3=1000;          % Liters per meter^3
+        visc=1E-3;          % Dynamic viscosity (water) [Pa s]
+		Adh=0.512; 			% L^1/2 / mol^1/2, approximate for room temperature
+		aD=1.5*sqrt(2); 	% mol^-1/2 mol^-3/2, approximation
+    end
+    
+    properties
+        ions;
+        concentrations;
+        pH;
+        % cond;
+		I=0;
+    end
+    
+    methods
+        
+        function obj=solution(ions, concentrations)
+			%Class Constructor
+            if(nargin == 2)
+                if isvector(ions) && all(strcmp(cellfun(@class, ions, 'UniformOutput', false), 'ion'))
+                    obj.ions=ions;
+                else
+                    error('You must input a vector of ions.')
+                end
+                if isvector(concentrations) && length(ions)==length(concentrations)
+					obj.concentrations=concentrations;
+                else
+                    error('The concentrations vector must be the same size as the ions vector.')
+                end
+                
+            else
+                error('Solutions must have a cell of ions and a cell of concentrations.')
+            end
+            
+            obj.pH=get_pH(obj);
+            % obj.cond=get_conductivity(obj);
+			obj.I=ionic_strength(obj, obj.pH);
+        end
+
+		function new_sol=add_ion(obj, new_ion, new_conc)
+			new_sol=solution(cat(2, obj.ions, {new_ion}), cat(2, obj.concentrations, new_conc));
+		end
+        
+        function pH=get_pH(obj, I)
+			% Finds the pH of the object. If an ionic strength is specified, 
+			% uses the corrected acidity constants. 
+			
+			% If ionic strength does not exist, assume it is zero.
+			% This function is used to find the equilibrium state, 
+			% so it cannot pull the ionic strength from the object.
+			if ~exist('I', 'var')
+				I=0; 
+			end
+			
+			%Set up concentration vector
+            cMat=obj.concentrations;
+			
+			% Find the order of the polynomial. This is the maximum
+			% size of the list of charge states in an ion. 
+            MaxCol=-inf;
+            for i=1:length(obj.concentrations)
+                MaxCol=max(MaxCol, max(obj.ions{i}.z)-min(obj.ions{i}.z)+1);
+            end
+            
+			% Set up the matrix of Ls, the multiplication
+			% of acidity coefficients for each ion.
+            LMat=zeros(length(cMat),MaxCol);
+            
+            for i=1:length(cMat)
+                L_list=obj.ions{i}.get_L(I);
+                Ip1=find(obj.ions{i}.z==1);     Im1=find(obj.ions{i}.z==-1);
+                L_list=[L_list(1:Im1),1,L_list(Ip1:end)];
+                LMat(i,1:length(obj.ions{i}.z)+1)=L_list;
+            end
+            
+        
+            % Construct Q 
+			% Made more consice by removing Q1 and Q2.  
+			Q=1;
+			% Convolve each line of the L matrix. 
+            for j=1:size(LMat,1)
+                Q=conv(Q,LMat(j,:));
+            end %for j
+			%Convolve with water dissociation.
+			Q=conv(Q, [-obj.Kw_eff(I) 0 1]);
+			
+			
+            
+            
+            %%%
+            %Construct P (done)
+            %%%
+%             PMat=zeros(obj.concentrations, 1)
+            for i=1:length(obj.concentrations)
+                z_list=obj.ions{i}.get_z0;
+                
+                tmp=zeros(1,size(LMat,2));
+                tmp(1:length(z_list))=z_list;
+                Mmod=LMat;     Mmod(i,:)=Mmod(i,:).*tmp;
+
+                Pi=1;
+                for kl=1:size(Mmod,1)
+                    Pi=conv(Pi,Mmod(kl,:));
+                end %for j
+                
+                Pi=conv([0 1],Pi);  % Convolve with P2
+                PMat(i,:)=Pi;
+            end %for i
+            
+            
+            %%%
+            %Pad Smaller Matrix
+            %%%
+            
+            SizeDiff=size(Q,2)-size(PMat,2);
+            if SizeDiff>0
+                PMat=[PMat,repmat(PMat(:,1)*0,1,SizeDiff)];
+            elseif SizeDiff<0
+                Q=[Q,repmat(0,1,SizeDiff)];
+            end
+                 
+         
+            %%%
+            %Construct Polynomial
+            %%%
+            cTotRep=cMat'*ones(1,size(PMat,2));
+
+            P=sum(cTotRep.*PMat,1);
+            
+            Poly=zeros(1,max(length(P), length(Q)));
+            Poly(1:length(P))=Poly(1:length(P))+P;
+            Poly(1:length(Q))=Poly(1:length(Q))+Q; %from QMat
+            
+            Poly=fliplr(Poly);
+            
+            %%%
+            % Solve Polynomial for concentration (should work)
+            %%%
+            roo=roots(Poly);
+            cH=roo(imag(roo)==0 & roo>0);
+            pH=-log10(cH);
+        end
+		
+		function [pH, I]=find_equilibrium(obj)
+			% Uses the fzero root finder to find the equilibrium
+			% pH and ionic strength of a solution, using the ionic-
+			% strength-adjusted activity coefficients.
+			
+			I=obj.ionic_strength(obj.get_pH);
+			I=fzero(@(x)equil_offset(obj, x), I);
+			pH=obj.get_pH(I);
+		end
+        
+		function Residual=equil_offset(obj,I_i)
+			% This function finds the offset between proposed
+			% pH and I and the pH and I calculated from the initial guess.
+			pH=obj.get_pH(I_i);
+			I_f=obj.ionic_strength(pH, I_i);
+			
+			Residual=(I_f-I_i);
+		end
+		
+        function cond=cond(obj)
+            %Calculates the conductivity of the solution. Does not
+            %currently include the conductivity contribution of protons or
+            %hydroxyls. 
+            cond=0;
+            for i=1:length(obj.concentrations)
+                cond=cond+obj.ions{i}.molar_conductivity(obj.pH)*obj.concentrations(i);
+            end
+            cond=cond+obj.F*obj.muH*obj.cH*obj.Lpm3; % H+ conductivity
+            cond=cond+obj.F*obj.muOH*obj.cOH*obj.Lpm3; % H+ conductivity
+        end
+        
+        function T=get_transference(obj)
+            %Gets the fraction of charge carried by each of the ions.
+            %Should not precisely add to 1, because some charge is carried
+            %by protons and hydroxyls. 
+            T=zeros(1,length(obj.ions));
+            for i=1:length(T)
+                T(i)=obj.ions{i}.molar_conductivity(obj.pH).*obj.concentrations(i);
+            end
+            T=T/obj.cond;
+        end
+        
+        function Qi=passage_charge(obj, LE)
+            Qi=0;
+        end
+        
+        function I=ionic_strength(obj, pH, I_guess)
+            % Calculates the ionic strength of the solution. This function
+            % does not account for changes in activity due to ionic strength
+            % effects. 
+			
+			% If a pH is not specified in the function call, use the pH
+			% of the object.
+			if ~exist('pH', 'var')
+				pH=obj.pH;
+			end
+			
+			if ~exist('I_guess', 'var') %calculate without an initial guess for ionic strength
+            	I=0;
+            	for i=1:length(obj.ions);
+            		I=I+obj.concentrations(i)*sum((double(obj.ions{i}.z).^2).*obj.ions{i}.ionization_fraction(pH));
+            	end
+				% Add the ionic strength due to water dissociation. 
+				I=I+obj.cH + obj.cOH;
+            	I=I/2;
+			else
+            	I=0;
+            	for i=1:length(obj.ions);
+            		I=I+obj.concentrations(i)*sum((double(obj.ions{i}.z).^2).*obj.ions{i}.ionization_fraction(obj.pH, I_guess));
+            	end
+				I=I+obj.cH + obj.cOH;
+            	I=I/2;
+			end
+        end
+		        
+        function Qi=zone_transfer(obj, vol)
+            %Calculates the zone transfer charge of the solution at a given
+            %volume. The volume of the solution is specified in liters. 
+            Qi=obj.concentrations./obj.get_transference.*vol.*obj.F;
+        end
+
+		function Cb=buffering_capacity(obj)
+			c=obj.concentrations(obj.concentrations>0);
+			c=0.01*min(c);
+			new_sol=obj.add_ion(ion('A-', -1, -2, -1), c);
+			Cb=abs(c/(obj.pH-new_sol.pH));
+		end
+		
+		function [mob_new, factor]=get_factor(obj)
+			
+			% Initialize the empty variables that you will need.
+			% Omega = mobility / F / z
+			omega=[];
+			% z_list is an unsorted list of all charges of all ions
+			z_list=[];
+			% conc_list is an unsorted list of all concentrations of all charge states. 
+			conc_list=[];
+			
+			%All three share the same order
+			
+			%populate them.
+			for i=1:length(obj.ions)
+				for j=1:length(obj.ions{i}.z)
+					z_list=cat(1, z_list, obj.ions{i}.z(j));
+					omega=cat(1, omega, obj.ions{i}.fi_mobility(j)/obj.F/z_list(end));
+				end
+				conc_list=cat(1, conc_list, (obj.concentrations(i).*obj.ions{i}.ionization_fraction(obj.pH))');	
+			end
+			
+			% Here is the mobility, which for some reason =mobility/F. 
+			mob=omega.*abs(z_list);
+			
+			n_states=length(omega);
+			mob_new=zeros(1,n_states);
+
+    
+			% mu is the (chemical?) potential of each ion.
+			mu=conc_list.*z_list.^2./obj.ionic_strength/2; %total potential
+			
+			
+			for j=1:n_states
+			    for i=1:n_states
+			   	h(j,i)=mu(i)*omega(i)/(omega(i)+omega(j));
+			    end
+			end
+			d=sum(h,2);
+			d=diag(d);
+			h=h+d;
+ 			II=eye(n_states,n_states);
+			 
+		    B=2*h-II;
+			
+		    r(:,1)=(z_list-sum(z_list.*mu)/sum(mu.*abs(z_list)./mob)*(abs(z_list)./mob))'; %check for absolute signs 
+
+		    for i=2:6                                        
+		    r(:,i)=B*r(:,i-1);
+			end
+			
+			c=[0.2929 -0.3536 0.0884 -0.0442 0.0276 -0.0193];
+			%coefficients in onsager-fuoss paper
+
+		    factor=c*r';
+			factor=factor';
+		    mob_new=obj.F*omega-(obj.F*0.78420*z_list.*factor.*omega+31.410e-9).*sqrt(obj.ionic_strength)./(1+1.5*sqrt(obj.ionic_strength));
+			mob_new=(mob_new.*z_list)';
+			
+			
+		end
+		
+		function Kw_eff=Kw_eff(obj, I)
+			% Gets the effective water dissociation constant
+			% due to activity corrections to H+ and OH-. 
+			if ~exist('I', 'var')
+				I=obj.I;
+			end
+			
+			% There are two coefficients that are used repeatedly.
+			% Specified in Bahga. 
+			A=obj.Adh*sqrt(I)/(1+obj.aD*sqrt(I));
+			B=0.1*I;
+			
+			% Use them to calculate the activity coefficients. 
+			% These coefficients are for z=+-1, for H+ and OH-
+			gam_h=B-A;
+			gam_h=10^gam_h;
+			Kw_eff=obj.Kw/gam_h^2;
+		end
+		
+		function cH=cH(obj)
+			% Supplies the concentration of H+ in the solution. 
+			cH=10^(-obj.pH);
+		end
+		
+		function cOH=cOH(obj)
+			% Supplies the concentration of OH- in the solution. 
+			cOH=obj.Kw_eff/obj.cH;
+		end
+			
+        
+    end %End methods section
+end %End class definition
+
+% Outdated code.
+
+% Q1=1;
+% for j=1:size(LMat,1)
+%     Q1=conv(Q1,LMat(j,:));
+% end %for j
+% Q2=[-obj.Kw_eff(I) 0 1];
+% Q=conv(Q1,Q2);
