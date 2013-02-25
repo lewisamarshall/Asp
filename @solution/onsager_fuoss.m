d2616b8662d2e57299ea15e8dc31898fb586d101
Lewis A. Marshall <lewis.a.marshall@gmail.com>
2013-02-24 18:28:36 -0800

    Bug fixes in Onsager_fouss.

--- @solution/onsager_fuoss.m
+++ @solution/onsager_fuoss.m
@@ -1,4 +1,4 @@
-function [mobility]=onsager_fuoss(obj)
+function [mobility, omega, z_list, conc_list]=onsager_fuoss(obj)
 			
 	% Initialize the empty variables that you will need.
 	% Omega = mobility / F / z
@@ -12,47 +12,45 @@ function [mobility]=onsager_fuoss(obj)
 			
 	%populate them.
 	for i=1:length(obj.ions)
-		omega=cat(1, omega, (obj.ions{i}.fi_mobility./obj.F./obj.ions{i}.z)');
-		z_list=cat(1, z_list, (obj.ions{i}.z)');
-		conc_list=cat(1, conc_list, (obj.concentrations(i).*obj.ions{i}.ionization_fraction(obj.pH, obj.I))');	
+		omega=cat(2, omega, (obj.ions{i}.fi_mobility./obj.F./obj.ions{i}.z));
+		z_list=cat(2, z_list, (obj.ions{i}.z));
+		conc_list=cat(2, conc_list, (obj.concentrations(i).*obj.ions{i}.ionization_fraction(obj.pH, obj.I)));	
 	end
 
 	n_states=length(omega);
 	
 	% potential is the (chemical?) potential of each ion.
-	potential=conc_list.*z_list.^2./obj.I/2; 
-	
+	potential=conc_list.*z_list.^2./(2*obj.I); 
 	% initialize and populate the h matrix. 
     h=zeros(n_states);
 	for j=1:n_states
 	    for i=1:n_states
-	   	h(j,i)=potential(i)*omega(i)/(omega(i)+omega(j));
+	   	h(i,j)=potential(i)*omega(i)/(omega(i)+omega(j));
 	    end
 	end
 	
 	d=diag(sum(h,2));
-	h=h+d;
-	II=eye(n_states);
-			 
-    B=2*h-II;
-			
-	% Here is the mobility, which for some reason =mobility/F. 
-	mob=omega.*abs(z_list);
-			
-    r(:,1) = (z_list-sum(z_list.*potential) / sum(potential.*abs(z_list)./mob) * (abs(z_list)./mob))'; %check for absolute signs 
+    B=2*(h+d)-eye(n_states);
 
+	r=zeros(n_states, 6);
+
+    r(:,1) = (z_list-sum(z_list.*potential) / sum(potential.*abs(z_list)./omega./abs(z_list)) * (1./omega))';
+	
     for i=2:6                                        
     	r(:,i)=B*r(:,i-1);
     end
-			
+
 	c=[0.2929 -0.3536 0.0884 -0.0442 0.0276 -0.0193];
 	%coefficients in onsager-fuoss paper
 
-    factor=(c*r')';
-    mob_new=omega-(0.78420*z_list.*factor.*omega+31.410e-9/obj.F).*sqrt(obj.I)./(1+1.5*sqrt(obj.I));
-	mob_new=(mob_new.*z_list*obj.F)';
-		
-		
+    factor=(c*r');
+
+	A_prime=obj.F*0.78420;
+	B_prime=31.41e-9;
+	
+	mob_new=obj.F*omega-(A_prime  *z_list.*factor.*omega+B_prime).*sqrt(obj.I)./(1+1.5*sqrt(obj.I));
+
+	mob_new=(mob_new.*z_list);
 		
 	index=1;
 	mobility=cell(1, length(obj.ions));
