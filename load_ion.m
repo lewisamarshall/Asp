fcf427d2265240677e9f4a67443351967a6f7655
Lewis A. Marshall <lewis.a.marshall@gmail.com>
2013-02-26 19:32:31 -0800

    Added a database, and a script to pull ions from the database.

---
+++ load_ion.m
@@ -0,0 +1,19 @@
+function [loaded_ion]=load_ion(ion_name)
+	% LOAD_ION is a function to pull an ion by name from a database.
+	% ion=LOAD_ION('ion name') pulls the named ion from the database.
+	% Database derived from Peakmaster.
+	
+	load database.mat
+	Z=[-3:-1, 1:3];
+	N=find(strcmpi(ion_name, NAME));
+	
+	if isempty(N)
+		error('Ion not found in database.')
+	end
+	
+	indices=~isnan(PKA(N, :));
+	loaded_ion=ion( NAME{N},...
+		Z(indices),...
+		PKA(N, indices),...
+		MOBILITY(N, indices).*1e-9);
+end
