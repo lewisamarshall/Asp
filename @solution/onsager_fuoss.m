3280f896b454d0babfda2177fb37f4acd72322bf
Lewis A. Marshall <lewis.a.marshall@gmail.com>
2013-02-28 00:52:46 -0800

    Fixed a bug where H+ and OH- mobilities are populated by the wrong adjusted mobility. Fixed a bug where zero mobility led to divide by zero error.

--- @solution/onsager_fuoss.m
+++ @solution/onsager_fuoss.m
@@ -30,10 +30,14 @@ function [mobility, omega, z_list, conc_list]=onsager_fuoss(obj)
     h=zeros(n_states);
 	for j=1:n_states
 	    for i=1:n_states
-	   	h(i,j)=potential(i)*omega(i)/(omega(i)+omega(j));
+		   	h(i,j)=potential(i)*omega(i)/(omega(i)+omega(j));
+			% Added to avoid NaN. Unsure if this is the correct strategy.
+			if isnan(h(i,j))
+				h(i,j)=potential(i)/2;
+			end
 	    end
 	end
-	
+	h
 	d=diag(sum(h,2));
     B=2*(h+d)-eye(n_states);
 
@@ -41,6 +45,13 @@ function [mobility, omega, z_list, conc_list]=onsager_fuoss(obj)
 
     r(:,1) = (z_list-sum(z_list.*potential) / sum(potential.*abs(z_list)./omega./abs(z_list)) * (1./omega))';
 	
+	% if r==NaN, r=0. 
+	for i=1:n_states
+		if isnan(r(i, 1))
+			r(i,1)=0;
+		end
+	end
+		
     for i=2:6                                        
     	r(:,i)=B*r(:,i-1);
     end
@@ -65,6 +76,6 @@ function [mobility, omega, z_list, conc_list]=onsager_fuoss(obj)
 		mobility{i}=mob_new(index:(index+length(obj.ions{i}.z)-1));
 		index=index+length(obj.ions{i}.z);
 	end
-	mobility{end}=mob_new(i:end);
+	mobility{end}=mob_new(index:end);
 
 end
