f22780b2c471ae6416658c522f83a4e25532ea83
Lewis A. Marshall <lewis.a.marshall@gmail.com>
2013-04-09 11:38:32 -0700

    Updated to use the effective charge on each molecule.

--- @solution/zone_transfer.m
+++ @solution/zone_transfer.m
@@ -1,5 +1,11 @@
 function Qi=zone_transfer(obj, vol)
     %Calculates the zone transfer charge of the solution at a given
     %volume. The volume of the solution is specified in liters. 
-    Qi=obj.concentrations./obj.get_transference.*vol.*obj.F;
+    Qi=zeros(1,length(obj.ions));
+	transference=obj.transference;
+    for i=1:length(Qi)
+        Qi(i)=obj.ions{i}.molar_conductivity(obj.pH, obj.I).*obj.concentrations(i)./transference(i)/abs(obj.ions{i}.effective_mobility(obj.pH, obj.I));
+    end
+	
+   	Qi=Qi*vol/obj.F/obj.Lpm3;
 end
